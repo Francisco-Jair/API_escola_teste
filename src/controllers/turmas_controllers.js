e3e791933a0f38b3155a72a07e69d93bccbdce25
Francisco Jair <fjair123@ufpi.edu.br>
2020-09-06 00:50:50 -0300

    Novas rotas

--- src/controllers/turmas_controllers.js
+++ src/controllers/turmas_controllers.js
@@ -21,4 +21,25 @@ module.exports = {
             next(error)
         }
     },
+    async insert_alunos(req, res, next) {
+        try {
+            const { users_id, turma_id } = req.body
+            await knex('turma_users').insert({ users_id, turma_id })
+            res.status(201).send({
+                msm: "Aluno Cadastrado"
+            })
+        } catch (error) {
+            next(error)
+        }
+    },
+    async list_alunos_turmas(req, res, next) {
+        try {
+            const id = req.params.id
+            const result = await knex('turma_users').where({ turma_id: id })
+
+            res.json(result)
+        } catch (error) {
+            next(error)
+        }
+    },
 };
--- src/routers/turmas_routers.js
+++ src/routers/turmas_routers.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const controllers_turmas = require('../controllers/turmas_controllers')
 
 router.get('/', controllers_turmas.list_turmas)
+router.get('/:id', controllers_turmas.list_alunos_turmas)
 router.post('/', controllers_turmas.create)
+router.post('/insert', controllers_turmas.insert_alunos)
 
 router.put('/', (req, res, next) => {
     res.status(201).send({
