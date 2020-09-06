9cb95c84fefb96c038ab3fd88e54f52ea4e3fbda
Francisco Jair <fjair123@ufpi.edu.br>
2020-09-05 22:44:38 -0300

    Migrates

---
+++ src/database/migrations/20200905222737_create_table_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', function (table) {
+        table.increments('id')
+        table.text('nome')
+        table.text('email')
+        table.integer('nome')
+
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+        table.timestamp('updated_at').defaultTo(knex.fn.now())
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('users')
+};
